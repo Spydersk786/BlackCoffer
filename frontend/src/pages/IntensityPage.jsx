92f1442e6818d4827bbdb92bd0d0a0ed6649a545
Spydersk786 <khuranashivam92@gmail.com>
2024-10-13 15:51:59 +0530

    hell yeahhh

--- frontend/src/pages/IntensityPage.jsx
+++ frontend/src/pages/IntensityPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import IntensityTop from '../components/IntensityTop';
+import Top from '../components/Top';
 import IntensityAggregation from '../components/IntensityAggregation';
 
 const IntensityPage = () => {
@@ -7,7 +7,7 @@ const IntensityPage = () => {
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-6">Intensity Analysis</h1>
       <div className="mb-8">
-        <IntensityTop />
+        <Top field='intensity'/>
       </div>
       <div>
         <IntensityAggregation />
